5ed82d60d08df23f4c498301a905aa77bdf16808
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-14 06:55:18 -0300

    :construction: Criando as rotas e importando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import styled, {createGlobalStyle} from "styled-components"
+import {createGlobalStyle} from "styled-components"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./Components/Header/Header"
+import Home from "./Components/Home/Home"
+import Novidades from "./Components/Novidades/Novidades"
+import Sobre from "./Components/Sobre/Sobre"
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -12,6 +17,16 @@ export default function App() {
   return (
     <>
     <GlobalStyle/>
+
+    <BrowserRouter>
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/novidades" element={<Novidades/>}/>
+        <Route path="/sobre" element={<Sobre/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
