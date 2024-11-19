f56cef869c657997986b317bff7eaefa3696127b
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 14:26:33 -0300

    :construction: Adicionando texto à página Sobre

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,7 +1,14 @@
+import { StyledSobre, TextoSobre, StyledH2, StyleP,  } from "./style"
+import loja from "../../assets/loja_sb.svg"
+
 export default function Sobre() {
     return (
-        <section>
-            <h2>Sobre</h2>
-        </section>
+        <StyledSobre>
+            <img src={loja} alt="Fachada de uma loja antiga da Starbucks" />
+            <TextoSobre>
+                <StyledH2>Niveis de Torra</StyledH2>
+                <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
+            </TextoSobre>
+        </StyledSobre>
     )
 }
