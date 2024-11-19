91b89ce7ae765f26e787060bcf3b1c03e3877030
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 12:32:53 -0300

    :construction: Adicionando texto da Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,14 @@
-import { MainStyle } from "./style"
+import { MainStyle, H2Style, H1Style, TextoSB } from "./style"
 
 export default function Home() {
     return (
         <MainStyle>
-            <h1>Home</h1>
+            <section>
+                <H2Style>Mais que Café</H2Style>
+                <H1Style>Isso é <TextoSB>Starbucks</TextoSB></H1Style>
+
+                <p>A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.</p>
+            </section>
             
         </MainStyle>
     )
