0f6205e59c76e7f8acc52dc2a47cd24518f628ea
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 09:04:19 -0300

    :lipstick: Estilizando a ul e links da Header

--- src/Components/Header/style.jsx
+++ src/Components/Header/style.jsx
@@ -2,16 +2,30 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const HeaderStyle = styled.header`
-display: flex;
-justify-content: space-between;
-background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    padding: 1.5em;
 `
 
 export const UlStyle = styled.ul`
-list-style-type: none;
-font-family: "Poppins";
+    list-style-type: none;
+    font-family: "Poppins";
+    display: flex;
+    justify-content: space-around;
+    width: 30vw;
 `;
 
 export const LinkStyle = styled(Link)`
     text-decoration: none;
+    color: #1E3932;
+
+    &:hover{
+        color: #037143;
+    }
+
+    &:visited{
+        color: #1E3932;
+    }
 `;
