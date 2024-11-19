1fbc4debcce9783468842e8326b7ecb716ebbebe
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 13:33:04 -0300

    :lipstick: Estilizando parágrafo

--- src/Components/Home/style.jsx
+++ src/Components/Home/style.jsx
@@ -3,21 +3,25 @@ import styled from "styled-components";
 export const MainStyle = styled.main`
     font-family: "Poppins";
     padding: 0 3em;
-`
+`;
 
 export const H2Style = styled.h2`
     font-family: "Inter";
     font-weight: 400;
     font-size: 64px;
-`
+`;
 
 export const H1Style = styled.h1`
     font-weight: 400;
     font-size: 64px;
-`
+`;
 
 export const TextoSB = styled.span`
     color: #037143;
     font-size: 90px;
     font-weight: 700;
-`
+`;
+
+export const P = styled.p`
+    width: 35vw;
+`;
