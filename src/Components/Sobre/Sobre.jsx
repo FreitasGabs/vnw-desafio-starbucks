26d2e1f5c151e52ec9e7dd0f047af939773b0d73
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 17:43:35 -0300

    :bug: Acertando um typo no texto

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -7,7 +7,7 @@ export default function Sobre() {
             <img src={loja} alt="Fachada de uma loja antiga da Starbucks" />
             <TextoSobre>
                 <StyledH2>Niveis de Torra</StyledH2>
-                <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
+                <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
             </TextoSobre>
         </StyledSobre>
     )
