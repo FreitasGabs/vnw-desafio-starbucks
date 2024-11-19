5079739216e45aaac012ffa73c804b1f4f922e70
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-19 14:27:03 -0300

    :lipstick: Estilizando a página Sobre

---
+++ src/Components/Sobre/style.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledSobre = styled.section`
+    font-family: "Poppins";
+
+`
+
+export const TextoSobre = styled.div`
+    width: 70vw;
+`
+
+export const StyledH2 = styled.h2`
+    font-weight: 400;
+    font-size: 64px;
+`;
+
+export const StyleP = styled.p`
+    width: 35vw;
+`;
