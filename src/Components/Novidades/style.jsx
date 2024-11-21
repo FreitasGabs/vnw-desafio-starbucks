bebbbbc7c077ed7907c2e13e1c0812f46f8e9905
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-21 16:55:36 -0300

    :lipstick: Estilizando a página Novidades

---
+++ src/Components/Novidades/style.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledNovidades = styled.section`
+    background-color: #1E3932;
+    color: white;
+    font-family: "Poppins";
+`
+
+export const TextoSobre = styled.div`
+    width: 70vw;
+`
+
+export const StyledH2 = styled.h2`
+    font-weight: 400;
+    font-size: 64px;
+`;
+
+export const StyleP = styled.p`
+    width: 35vw;
+`;
