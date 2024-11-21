6a0328687a5c93777559e3f508f44fe51e8b3f25
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-21 16:55:18 -0300

    :construction: Adicionando texto à página Novidades

--- src/Components/Novidades/Novidades.jsx
+++ src/Components/Novidades/Novidades.jsx
@@ -1,7 +1,15 @@
+import { StyledNovidades, TextoSobre, StyledH2, StyleP } from "./style"
+import graoCafe from "../../assets/grao_cafe.svg"
+
 export default function Novidades() {
     return (
-        <section>
-            <h2>Novidades</h2>
-        </section>
+        <StyledNovidades>
+            <TextoSobre>
+                <StyledH2>Niveis de Torra</StyledH2>
+                <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
+            </TextoSobre>
+
+            <img src={graoCafe} alt="Um grão de café dividido em três tipos diferentes, verticalmente" />
+        </StyledNovidades>
     )
 }
