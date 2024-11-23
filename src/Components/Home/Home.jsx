e614342f780562100b24a8dda26697246d4d94c5
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-23 16:07:20 -0300

    :wrench: Fazendo a lógica para troca de copos

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,30 @@
-import { MainStyle, H2Style, H1Style, TextoSB, StyleP } from "./style"
+import { MainStyle, H2Style, H1Style, TextoSB, StyleP, StyledButton } from "./style"
+import copinhoAmarelo from "../../assets/cafe_amarelo.svg"
+import copinhoLaranja from "../../assets/cafe_laranja.svg"
+import copinhoVermelho from "../../assets/cafe_vermelho.svg"
+import copoAmarelo from "../../assets/cafe_amarelo2x.svg"
+import copoLaranja from "../../assets/cafe_laranja2x.svg"
+import copoVermelho from "../../assets/cafe_vermelho2x.svg"
+import { useState } from "react"
 
 export default function Home() {
+
+    const [copo, setCopo] = useState(copoLaranja)
+
+    const trocaAmarelo = () => {
+        setCopo(copoAmarelo)
+    }
+
+    const trocaLaranja = () => {
+        setCopo(copoLaranja)
+    }
+
+    const trocaVermelho = () => {
+        setCopo(copoVermelho)
+    }
+
+
+
     return (
         <MainStyle>
             <section>
@@ -8,6 +32,18 @@ export default function Home() {
                 <H1Style>Isso é <TextoSB>Starbucks</TextoSB></H1Style>
 
                 <StyleP>A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.</StyleP>
+
+                <StyledButton>Saiba Mais</StyledButton>
+
+                <section>
+                    <img src={copinhoLaranja} onClick={trocaLaranja} alt="" />
+                    <img src={copinhoAmarelo} onClick={trocaAmarelo} alt="" />
+                    <img src={copinhoVermelho} onClick={trocaVermelho} alt="" />
+                </section>
+            </section>
+
+            <section>
+                <img src={copo} alt="" />
             </section>
             
         </MainStyle>
