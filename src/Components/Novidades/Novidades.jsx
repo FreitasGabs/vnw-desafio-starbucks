3972cd521feee8fc1b1091c99dfb555364e28c4a
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-11-25 10:22:10 -0300

    :construction: Criando o botão de Saiba Mais

--- src/Components/Novidades/Novidades.jsx
+++ src/Components/Novidades/Novidades.jsx
@@ -1,4 +1,4 @@
-import { StyledNovidades, TextoSobre, StyledH2, StyleP } from "./style"
+import { StyledNovidades, TextoSobre, StyledH2, StyleP, StyledButton } from "./style"
 import graoCafe from "../../assets/grao_cafe.svg"
 
 export default function Novidades() {
@@ -7,6 +7,7 @@ export default function Novidades() {
             <TextoSobre>
                 <StyledH2>Niveis de Torra</StyledH2>
                 <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
+                <StyledButton>SAIBA MAIS</StyledButton>
             </TextoSobre>
 
             <img src={graoCafe} alt="Um grão de café dividido em três tipos diferentes, verticalmente" />
--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,4 +1,4 @@
-import { StyledSobre, TextoSobre, StyledH2, StyleP,  } from "./style"
+import { StyledSobre, TextoSobre, StyledH2, StyleP, StyledButton } from "./style"
 import loja from "../../assets/loja_sb.svg"
 
 export default function Sobre() {
@@ -8,6 +8,7 @@ export default function Sobre() {
             <TextoSobre>
                 <StyledH2>Niveis de Torra</StyledH2>
                 <StyleP>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®</StyleP>
+                <StyledButton>SAIBA MAIS</StyledButton>
             </TextoSobre>
         </StyledSobre>
     )
